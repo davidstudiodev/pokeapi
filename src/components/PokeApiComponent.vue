910ad5b0df014e01f9f93d44c4ad557f8d970523
David <davidstudiosco@gmail.com>
2024-12-18 13:04:13 +0100

    Project working.

--- src/components/PokeApiComponent.vue
+++ src/components/PokeApiComponent.vue
@@ -2,7 +2,7 @@
     <section class="bg-zinc-800 h-screen flex flex-col items-center p-20">
         
         <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="">
-        <h1 class="text-2xl text-white p-10 flex text-center">Find you<br>Pokemon</h1>
+        <h1 class="text-2xl text-white p-10 flex text-center">Find your Pokemon</h1>
         
         <div class="flex items-center gap-5 flex-col md:flex-row">
             <input type="text" class="rounded-xl h-10 w-60 p-5 outline-none" placeholder="Write.." v-model="poke_input">
@@ -10,7 +10,7 @@
         </div>
         
         <div class="text-white">
-            <img class="border h-60 mt-10 w-60" :src="image" alt="">
+            <img class="border h-60 mt-10 w-60 bg-yellow-400" :src="image" alt="">
             <div class="w-60 p-4 flex flex-col items-center">
                 <h3 class="border p-2 w-60">Name: <strong class="text-yellow-300">{{ name }}</strong></h3>
                 <p class="border p-2 w-60">Type: <strong class="text-green-300">{{ type }}</strong></p>
@@ -30,6 +30,13 @@ import { useToast } from "vue-toastification";
 
 export default {
 name: 'PokeApi',
+
+setup() {
+    // Get toast interface
+    const toast = useToast();
+    return { toast }    
+},
+
 data() {
     return{
         poke_input: '',
@@ -38,25 +45,10 @@ data() {
         image: '',
         moves: '',
         showPoke: false,
+        
     }
 },
 
-setup() {
-    // Get toast interface
-    const toast = useToast();
-
-    // Use it!
-    toast("I'm a toast!");
-
-    // or with options
-    toast.success("My toast content", {
-        timeout: 2000
-    });
-    // These options will override the options defined in the "app.use" plugin registration for this specific toast
-
-    // Make it available inside methods
-    return { toast }
-},
 methods: {
     FindPokemon(){
         fetch(`https://pokeapi.co/api/v2/pokemon/${this.poke_input}`)
@@ -67,7 +59,16 @@ methods: {
             this.type = data.types[Math.floor(Math.random() * data.types.length)].type.name
             this.moves = data.moves[Math.floor(Math.random() * data.moves.length)].move.name
             this.showPoke = true
+    
         })
+    
+    .catch(() => {
+        this.toast.error('Something went wrong! Pokémon not found.', 
+        {position: 'bottom-center'
+        })
+        this.showPoke = false
+    })    
+
     }
     
 },
