ce1f44cf76a3557d4f7a5779ec5c7934d298cffa
David <davidstudiosco@gmail.com>
2024-12-18 12:33:13 +0100

    Add Toastiy

--- src/components/PokeApiComponent.vue
+++ src/components/PokeApiComponent.vue
@@ -1,16 +1,20 @@
 <template>
     <section class="bg-zinc-800 h-screen flex flex-col items-center p-20">
+        
+        <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="">
         <h1 class="text-2xl text-white p-10 flex text-center">Find you<br>Pokemon</h1>
-        <div class="flex gap-5 flex-col md:flex-row">
+        
+        <div class="flex items-center gap-5 flex-col md:flex-row">
             <input type="text" class="rounded-xl h-10 w-60 p-5 outline-none" placeholder="Write.." v-model="poke_input">
             <button class="bg-white p-2 rounded-xl w-24" @click="FindPokemon">Find</button>
         </div>
+        
         <div class="text-white">
-            <img class="bg-white h-48" :src="image" alt="">
-            <div class="w-60 p-4">
-                <h3 class="border">Name: {{ name }}</h3>
-                <p class="border">Type: {{ type }}</p>
-                <p class="border">Moves: {{ moves }}</p>
+            <img class="border h-60 mt-10 w-60" :src="image" alt="">
+            <div class="w-60 p-4 flex flex-col items-center">
+                <h3 class="border p-2 w-60">Name: <strong class="text-yellow-300">{{ name }}</strong></h3>
+                <p class="border p-2 w-60">Type: <strong class="text-green-300">{{ type }}</strong></p>
+                <p class="border p-2 w-60">Moves: <strong class="text-blue-400">{{ moves }}</strong></p>
             </div>
         </div>
 
@@ -19,6 +23,11 @@
 </template>
 
 <script>
+
+import { useToast } from "vue-toastification";
+
+
+
 export default {
 name: 'PokeApi',
 data() {
@@ -28,7 +37,25 @@ data() {
         name: '',
         image: '',
         moves: '',
+        showPoke: false,
     }
+},
+
+setup() {
+    // Get toast interface
+    const toast = useToast();
+
+    // Use it!
+    toast("I'm a toast!");
+
+    // or with options
+    toast.success("My toast content", {
+        timeout: 2000
+    });
+    // These options will override the options defined in the "app.use" plugin registration for this specific toast
+
+    // Make it available inside methods
+    return { toast }
 },
 methods: {
     FindPokemon(){
@@ -38,6 +65,8 @@ methods: {
             this.image = data.sprites.front_default
             this.name = data.name
             this.type = data.types[Math.floor(Math.random() * data.types.length)].type.name
+            this.moves = data.moves[Math.floor(Math.random() * data.moves.length)].move.name
+            this.showPoke = true
         })
     }
     
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
-createApp(App).mount('#app')
+createApp(App).use(Toast).mount('#app')
